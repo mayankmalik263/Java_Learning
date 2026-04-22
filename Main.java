22fcf054733eb88379bf07f44254cbd356ba707b
mayankmalik263 <mayankmalik263@gmail.com>
2026-04-23 00:44:09 +0530

    as we can see that we can use byte to store age of a person more efficiently and then when we give a large value to an int value it is showing an error.

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class Main{
         int age = 20;
         age = 30;
         System.out.println(age);
+        // we can also use different primitive types to store values for certain 
+        byte hisAge = 10;
+        int viewsCount = 3_123456789;
+
     }
 } 
